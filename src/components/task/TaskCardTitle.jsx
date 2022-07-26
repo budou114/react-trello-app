fa97c1d1c0bc8b9e703064a38198d17fce75a5de
budou114 <58349030+budou114@users.noreply.github.com>
2022-07-26 16:34:34 +0900

    CardTitleコンポーネントの実装

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const TaskCardTitle = () => {
+  const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setInputCardTitle] = useState("Today");
+  const handleClick = () => {
+    setIsClick(true);
+  };
+
+  const handleChange = (e) => {
+    setInputCardTitle(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClick(false);
+  };
+
+  const handleBlur = () => {
+    setIsClick(false);
+  }
+
   return (
-    <div>TaskCardTitle</div>
-  )
-}
+    <div onClick={handleClick}>
+      { isClick ? (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={inputCardTitle}
+            autoFocus
+            maxLength="10"
+          />
+        </form>
+      ) : (
+      <h3>{inputCardTitle}</h3>
+    ) } 
+    </div>
+  );
+};
