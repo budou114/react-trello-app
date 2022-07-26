714ff7be6bc78a935e0c3a27945f1f22b7a02523
budou114 <58349030+budou114@users.noreply.github.com>
2022-07-26 17:07:06 +0900

    classコンポーネントになっているため、関数コンポーネントに変更

--- src/components/task/TaskCard.jsx
+++ src/components/task/TaskCard.jsx
@@ -1,18 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { TaskCardDeleteButton } from './button/TaskCardDeleteButton'
 import { TaskAddInput } from './input/TaskAddInput'
 import { TaskCardTitle } from './TaskCardTitle'
 import { Tasks } from './Tasks'
 
-export default class TaskCard extends Component {
-  render() {
-    return (
-      <div className='taskCard'>
-        <TaskCardTitle />
-        <TaskCardDeleteButton />
-        <TaskAddInput />
-        <Tasks />
-      </div>
-    )
-  }
+export const TaskCard = () => {
+  return (
+    <div className='taskCard'>
+      <TaskCardTitle />
+      <TaskCardDeleteButton />
+      <TaskAddInput />
+      <Tasks />
+    </div>
+  )
 }
--- src/components/task/TaskCards.jsx
+++ src/components/task/TaskCards.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AddTaskCardButton } from './button/AddTaskCardButton'
-import TaskCard from './TaskCard'
+import { TaskCard } from './TaskCard'
 
 export const TaskCards = () => {
   return (
