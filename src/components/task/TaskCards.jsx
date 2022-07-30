c137ef5102d82074d28b8a34224b1ade4c984b6d
budou114 <58349030+budou114@users.noreply.github.com>
2022-07-30 15:19:09 +0900

    並び替えが戻るバグを修正

--- src/components/task/TaskCards.jsx
+++ src/components/task/TaskCards.jsx
@@ -3,6 +3,12 @@ import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 import { AddTaskCardButton } from './button/AddTaskCardButton';
 import { TaskCard } from './TaskCard';
 
+const reorder = (taskCardsList, startIndex, endIndex) => {
+  // タスクを並び替える
+  const remove = taskCardsList.splice(startIndex, 1) //[2, 3]
+  taskCardsList.splice(endIndex, 0, remove[0]); //[2, 1, 3]
+}
+
 export const TaskCards = () => {
   const [taskCardsList, setTaskCardsList] = useState([
     {
@@ -11,8 +17,13 @@ export const TaskCards = () => {
     }
   ]);
 
+  const handleDragEnd = (result) => {
+    reorder(taskCardsList, result.source.index, result.destination.index);
+    setTaskCardsList(taskCardsList);
+  };
+
   return (
-    <DragDropContext>
+    <DragDropContext onDragEnd={handleDragEnd}>
       <Droppable droppableId='droppable' direction='horizon'>
         {(provided) => (
           <div
