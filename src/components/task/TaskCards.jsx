052300a4b8d03802f97a3c2d59dfa3f340bff84c
budou114 <58349030+budou114@users.noreply.github.com>
2022-07-30 11:19:01 +0900

    初期値の設定

--- src/components/task/TaskCards.jsx
+++ src/components/task/TaskCards.jsx
@@ -3,7 +3,13 @@ import { AddTaskCardButton } from './button/AddTaskCardButton';
 import { TaskCard } from './TaskCard';
 
 export const TaskCards = () => {
-  const [taskCardsList, setTaskCardsList] = useState([]);
+  const [taskCardsList, setTaskCardsList] = useState([
+    {
+      id: 0,
+      draggableId: 'item0'
+    }
+  ]);
+
   return (
     <div className='taskCardsArea'>
       {taskCardsList.map(taskCardList => (
